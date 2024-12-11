f4704d012b686fbd4ab33ac00ef86540e80c522c
Pieterwerff <56294298+Pieterwerff@users.noreply.github.com>
2024-12-11 12:09:34 +0100

    afbeeldingen + leidraad naar tekst

--- run.py
+++ run.py
@@ -2,22 +2,26 @@ from openai import OpenAI
 import os 
 from dotenv import load_dotenv
 import pymupdf4llm
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
 md_text = pymupdf4llm.to_markdown("Leidraad kwaliteit AI in de zorg NL.pdf")
-print(md_text)
+f = open("leidraad.txt", "w")
+f.write(md_text)
+
 load_dotenv()
 OpenAI.api_key = os.getenv('OPENAI_API_KEY')
-                           
-client = OpenAI()
+# client = OpenAI()
 
-completion = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-        {"role": "system", "content": f"Je beantwoord vragen over het document Leidraad in de zorg. Baseer je antwoord op de volgende tekst: {md_text}"},
-        {
-            "role": "user",
-            "content": "Waar gaat het document over? "
-        }
-    ]
-)
+# completion = client.chat.completions.create(
+#     model="gpt-4o-mini",
+#     messages=[
+#         {"role": "system", "content": f"Je beantwoord vragen over het document Leidraad in de zorg. Baseer je antwoord op de volgende tekst: {md_text}"},
+#         {
+#             "role": "user",
+#             "content": "Waar gaat het document over? "
+#         }
+#     ]
+# )
 
-print(completion.choices[0].message.content)
+# print(completion.choices[0].message.content)
