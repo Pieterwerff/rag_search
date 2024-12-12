a38e8ecfa99335344cd4e53c51f2dfbe71a78e71
Pieterwerff <56294298+Pieterwerff@users.noreply.github.com>
2024-12-12 12:41:56 +0100

    soort push

--- run.py
+++ run.py
@@ -4,22 +4,27 @@ from dotenv import load_dotenv
 import pymupdf4llm
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import chromadb
+from llama_index.vector_stores.chroma import ChromaVectorStore
 
-md_text = pymupdf4llm.to_markdown("Leidraad kwaliteit AI in de zorg NL.pdf")
-f = open("leidraad.txt", "w")
-f.write(md_text)
+# documents = pymupdf4llm.to_markdown("Leidraad kwaliteit AI in de zorg NL.pdf")
+md_text = open("leidraad.txt")
+# f = open("leidraad.txt", "w")
+# f.write(documents)
 
 load_dotenv()
 OpenAI.api_key = os.getenv('OPENAI_API_KEY')
-# client = OpenAI()
+client = OpenAI()
+
+
 
 # completion = client.chat.completions.create(
 #     model="gpt-4o-mini",
 #     messages=[
-#         {"role": "system", "content": f"Je beantwoord vragen over het document Leidraad in de zorg. Baseer je antwoord op de volgende tekst: {md_text}"},
+#         {"role": "system", "content": f"Je beantwoordt vragen over het document Leidraad in de zorg. Baseer je antwoord op de volgende tekst: {md_text}"},
 #         {
 #             "role": "user",
-#             "content": "Waar gaat het document over? "
+#             "content": "Waar staat AIPA voor? "
 #         }
 #     ]
 # )
