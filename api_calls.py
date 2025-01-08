a29d0eb64bff5a2ffac223579cb5d4c05467a27f
Pieterwerff <56294298+Pieterwerff@users.noreply.github.com>
2025-01-08 17:03:59 +0100

    Modulair maken van de tool

---
+++ api_calls.py
@@ -0,0 +1,30 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+# Load environment variables and initialize OpenAI client
+load_dotenv()
+OpenAI.api_key = os.getenv('OPENAI_API_KEY')
+
+def query_llm(retrieved_text, user_query, llm):
+    if llm == 'gpt':
+        client = OpenAI()
+    """
+    Queries the LLM with a user query and relevant text.
+
+    Parameters:
+        retrieved_text (str): Relevant chunk retrieved from ChromaDB.
+        user_query (str): User's input question.
+
+    Returns:
+        str: Response from the LLM.
+    """
+    print(retrieved_text)
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "Je bent een assistent die vragen over de Leidraad AI in de zorg beantwoordt."},
+            {"role": "user", "content": f"Baseer je antwoord op deze tekst: {retrieved_text}\n\n{user_query}"}
+        ]
+    )
+    return response.choices[0].message.content
